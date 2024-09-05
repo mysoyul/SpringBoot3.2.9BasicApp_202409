dba7fcb6a0977ded4222ef03101a8978d09a8891
mysoyul <vega2k_@naver.com>
2024-09-05 14:09:57 +0900

    myboot props add

--- src/main/java/com/basic/myspringboot/runner/MyRunner.java
+++ src/main/java/com/basic/myspringboot/runner/MyRunner.java
@@ -1,12 +1,21 @@
 package com.basic.myspringboot.runner;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyRunner implements ApplicationRunner {
+    @Value("${myboot.name}")
+    private String name;
+
+    @Value("${myboot.age}")
+    private int age;
+
+
     public void run(ApplicationArguments args) throws Exception {
+
         System.out.println("Program Arguments foo : " + args.containsOption("foo"));
         System.out.println("VM Arguments bar : " + args.containsOption("bar"));
 
