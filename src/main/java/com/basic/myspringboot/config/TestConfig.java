2ef515462c8f7a42f50e92960ee8d87d08d5efd6
mysoyul <vega2k_@naver.com>
2024-09-05 15:25:31 +0900

    spring.profiles.active props add

--- src/main/java/com/basic/myspringboot/config/TestConfig.java
+++ src/main/java/com/basic/myspringboot/config/TestConfig.java
@@ -6,13 +6,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("prod")
-public class ProdConfig {
+@Profile("test")
+public class TestConfig {
     @Bean
     public CustomerVO customer() {
         return CustomerVO.builder() //CustomerVOBuilder
-                .id(200L)
-                .mode("운영환경")
+                .id(100L)
+                .mode("개발환경")
                 .build();
     }
 }
