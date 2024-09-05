d1a0641d7575ea24268bb1708afe8f0597936782
mysoyul <vega2k_@naver.com>
2024-09-05 13:50:54 +0900

    MyRunner ApplicationArguments update

--- src/main/java/com/basic/myspringboot/runner/MyRunner.java
+++ src/main/java/com/basic/myspringboot/runner/MyRunner.java
@@ -7,7 +7,19 @@ import org.springframework.stereotype.Component;
 @Component
 public class MyRunner implements ApplicationRunner {
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println("foo : " + args.containsOption("foo"));
-        System.out.println("bar : " + args.containsOption("bar"));
+        System.out.println("Program Arguments foo : " + args.containsOption("foo"));
+        System.out.println("VM Arguments bar : " + args.containsOption("bar"));
+
+
+        args.getOptionNames() //Set<String>
+                //forEach(Consumer) void accept(T t)
+                .forEach(name -> System.out.println("아규먼트 Name = " + name));
+                //.forEach(System.out::println);
+
+        if(args.containsOption("spring.application.name")){
+            args.getOptionValues("spring.application.name")
+                    .forEach(value -> System.out.println("spring.application.name Value = " + value));
+        }
+
     }
 }
