e5da031d0bd2d8bc42b454f7892600690a0b546d
mysoyul <vega2k_@naver.com>
2024-09-05 14:18:03 +0900

    Environment use

--- src/main/java/com/basic/myspringboot/runner/MyRunner.java
+++ src/main/java/com/basic/myspringboot/runner/MyRunner.java
@@ -1,8 +1,10 @@
 package com.basic.myspringboot.runner;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,8 +15,13 @@ public class MyRunner implements ApplicationRunner {
     @Value("${myboot.age}")
     private int age;
 
+    @Autowired
+    private Environment environment;
 
     public void run(ApplicationArguments args) throws Exception {
+        System.out.println("${myboot.name} = " + name);
+        System.out.println("${myboot.age} = " + age);
+        System.out.println("${myboot.fullName} = " + environment.getProperty("myboot.fullName"));
 
         System.out.println("Program Arguments foo : " + args.containsOption("foo"));
         System.out.println("VM Arguments bar : " + args.containsOption("bar"));
