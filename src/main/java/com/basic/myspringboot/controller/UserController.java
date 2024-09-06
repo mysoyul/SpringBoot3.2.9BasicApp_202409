f96e67ec04ccd1e19d1d94afe378cab8041927e4
mysoyul <vega2k_@naver.com>
2024-09-06 13:12:44 +0900

    UserController add

--- src/main/java/com/basic/myspringboot/controller/UserController.java
+++ src/main/java/com/basic/myspringboot/controller/UserController.java
@@ -1,14 +1,27 @@
 package com.basic.myspringboot.controller;
 
+import com.basic.myspringboot.entity.User;
 import com.basic.myspringboot.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/users")
 public class UserController {
     private final UserRepository userRepository;
 
     //Constructor Injection
-    public UserController(UserRepository userRepository) {
-        this.userRepository = userRepository;
+//    public UserController(UserRepository userRepository) {
+//        this.userRepository = userRepository;
+//    }
+
+    @PostMapping
+    public User create(@RequestBody User user) {
+        return userRepository.save(user);
     }
+
 }
