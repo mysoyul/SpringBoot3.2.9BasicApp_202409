c6e1e058861594e5bad3c68761a89322518e43f8
mysoyul <vega2k_@naver.com>
2024-09-06 09:20:00 +0900

    finder method add

--- src/main/java/com/basic/myspringboot/repository/CustomerRepository.java
+++ src/main/java/com/basic/myspringboot/repository/CustomerRepository.java
@@ -3,6 +3,8 @@ package com.basic.myspringboot.repository;
 import com.basic.myspringboot.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CustomerRepository extends JpaRepository<Customer,Long> {
+import java.util.Optional;
 
+public interface CustomerRepository extends JpaRepository<Customer,Long> {
+    Optional<Customer> findByCustomerId(String custId);
 }
