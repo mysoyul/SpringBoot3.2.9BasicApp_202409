16df8d1338b8370fb4a3ea6adb2402d6e07636fa
mysoyul <vega2k_@naver.com>
2024-09-06 15:34:19 +0900

    UserController add

--- src/main/java/com/basic/myspringboot/controller/UserController.java
+++ src/main/java/com/basic/myspringboot/controller/UserController.java
@@ -1,58 +1,15 @@
 package com.basic.myspringboot.controller;
 
-import com.basic.myspringboot.entity.User;
-import com.basic.myspringboot.exception.BusinessException;
-import com.basic.myspringboot.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.List;
-
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("/api/users")
+@Controller
 public class UserController {
-    private final UserRepository userRepository;
 
-    //Constructor Injection
-//    public UserController(UserRepository userRepository) {
-//        this.userRepository = userRepository;
-//    }
-
-    @PostMapping
-    public User create(@RequestBody User user) {
-        return userRepository.save(user);
+    @GetMapping("/thymeleaf")
+    public String leaf(Model model) {
+        model.addAttribute("name", "스프링부트");
+        return "leaf";
     }
-
-    @GetMapping("/{email}")
-    public User getUser(@PathVariable String email){
-        return getUserByEmail(email);
-    }
-
-    private User getUserByEmail(String email) {
-        return userRepository.findByEmail(email)   //Optional<User>
-                .orElseThrow(() -> new BusinessException(email + " User Not Found", HttpStatus.NOT_FOUND));
-    }
-
-    @GetMapping
-    public List<User> getUsers() {
-        return userRepository.findAll();
-    }
-
-    @PatchMapping("/{email}")
-    public User updateUser(@PathVariable String email, @RequestBody User userDetail) {
-        User user = getUserByEmail(email);
-        user.setName(userDetail.getName());
-        return userRepository.save(user);
-    }
-
-    @DeleteMapping("/{email}")
-    public ResponseEntity<?> deleteUser(@PathVariable String email) {
-        User user = getUserByEmail(email);
-        userRepository.delete(user);
-        return ResponseEntity.ok(email + " User Deleted OK!");
-    }
-
 }
---
+++ src/main/java/com/basic/myspringboot/controller/UserRestController.java
@@ -0,0 +1,58 @@
+package com.basic.myspringboot.controller;
+
+import com.basic.myspringboot.entity.User;
+import com.basic.myspringboot.exception.BusinessException;
+import com.basic.myspringboot.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+public class UserRestController {
+    private final UserRepository userRepository;
+
+    //Constructor Injection
+//    public UserController(UserRepository userRepository) {
+//        this.userRepository = userRepository;
+//    }
+
+    @PostMapping
+    public User create(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @GetMapping("/{email}")
+    public User getUser(@PathVariable String email){
+        return getUserByEmail(email);
+    }
+
+    private User getUserByEmail(String email) {
+        return userRepository.findByEmail(email)   //Optional<User>
+                .orElseThrow(() -> new BusinessException(email + " User Not Found", HttpStatus.NOT_FOUND));
+    }
+
+    @GetMapping
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    @PatchMapping("/{email}")
+    public User updateUser(@PathVariable String email, @RequestBody User userDetail) {
+        User user = getUserByEmail(email);
+        user.setName(userDetail.getName());
+        return userRepository.save(user);
+    }
+
+    @DeleteMapping("/{email}")
+    public ResponseEntity<?> deleteUser(@PathVariable String email) {
+        User user = getUserByEmail(email);
+        userRepository.delete(user);
+        return ResponseEntity.ok(email + " User Deleted OK!");
+    }
+
+}
